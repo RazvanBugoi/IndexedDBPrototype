98e83c3105310b81129d925d1318ef12d7445110
RazvanBugoi <46108577+RazvanBugoi@users.noreply.github.com>
2020-07-30 19:33:45 +0100

    changed object to array to store all the items

--- script.js
+++ script.js
@@ -2,29 +2,9 @@ let notes = document.getElementById('insert');
 let add = document.getElementById('add');
 let list = document.getElementById('list');
 
-let prototype = {
-    title: null,
-    text: 'this is my first note',
-    date: new Date().getDate(),
-    time: new Date().getTime()
-};
+let prototype = []; 
 
 const request = indexedDB.open('Database', 1);
-
-function addNotes() {
-    let tx = database.transaction('personal_notes', 'readonly'); //readwrite ca sa pot folosi metoda .add mai jos
-    let myNote = tx.objectStore('personal_notes');
-    let request = myNote.openCursor()
-    
-    request.onsuccess = (e) => {
-        let cursor = e.target.result;
-        if (cursor) {
-            cursor.continue();
-        } else {
-           console.log('Cursor is null'); 
-        }
-    }
-}
 
 request.onupgradeneeded = function(e) {
     database = e.target.result;
@@ -40,6 +20,29 @@ request.onerror = function(error) {
     alert('error');
 }
 
+function addNotes() {
+    let tx = database.transaction('personal_notes', 'readonly'); 
+    let myNote = tx.objectStore('personal_notes');
+    let request = myNote.openCursor()
+    
+    obj = {
+        title: null,
+        text: 'this is my first note',
+        date: new Date().getDate(),
+        time: new Date().getTime()
+    };
+
+    request.onsuccess = (e) => {
+        let cursor = e.target.result;
+        if (cursor) {
+
+            cursor.continue();
+        } else {
+           
+        }
+    }
+}
+
 
 add.onclick = insertNote 
 
@@ -48,6 +51,10 @@ function insertNote() {
     let info = document.createElement('li');
     info.innerHTML = `${notes.value}`;
     prototype.title = `${notes.value}`;
+
+    let tx = database.transaction('personal_notes', 'readwrite');
+    let myPost = tx.objectStore('personal_notes');
+    myPost.add(prototype);
     console.log(prototype);
     list.appendChild(info);
 }
